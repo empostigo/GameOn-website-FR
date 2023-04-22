19eb869d06243f0cb17a865820888f67816a553d
empostigo <emmanuel.postigo@tutanota.com>
2023-04-22 11:02:59 +0200

    Enable close button of the modal form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,22 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Get the close button element
+const closeBtn = document.querySelector(".close")
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Wait for close event
+closeBtn.addEventListener("click", closeModal)
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none"    // Set display to none
+}
 
